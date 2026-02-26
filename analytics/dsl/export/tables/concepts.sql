bb9fb753d2b74a2ee0eab81ac0a3e30b04479d43
Emmanuel Nyachoke <achachiez@gmail.com>
2026-02-26 13:52:43 +0300

    C2C-439: Fix found when deploying the provider changes for C2C

--- analytics/dsl/export/tables/concepts.sql
+++ analytics/dsl/export/tables/concepts.sql
@@ -1,11 +1,11 @@
 CREATE TABLE concepts (
-    concept_id BIGINT,
-    concept_mapping_source VARCHAR,
-    concept_mapping_code VARCHAR,
-    concept_mapping_name VARCHAR,
-    name VARCHAR,
-    locale VARCHAR,
+    concept_id BIGINT NOT NULL,
+    concept_mappings_source_codes STRING,
+    name STRING,
+    locale STRING,
     locale_preferred BOOLEAN,
     retired BOOLEAN,
-    uuid VARCHAR
+    uuid STRING,
+    answer_concepts_uuids STRING,
+    member_concepts_uuids STRING
 )
