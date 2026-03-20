941f7c5ecf6dad9eebc2c57eb361f5495dad8e21
Emmanuel Nyachoke <achachiez@gmail.com>
2026-03-20 07:33:19 +0300

    MG-29: Add facility id to FHIR flattened resources

--- analytics/fhir-data-pipes/views/Patient_flat.sql
+++ analytics/fhir-data-pipes/views/Patient_flat.sql
@@ -3,7 +3,12 @@ SELECT P.id AS id, P.active, PN.family, PNG AS given, P.gender,
   P.deceased.Boolean AS deceased,
   YEAR(current_date()) - YEAR(P.birthDate) AS age,
   PA.country, PG.practitionerId AS practitioner_id,
-  P.managingOrganization.organizationId AS organization_id
+  P.managingOrganization.organizationId AS organization_id,
+  regexp_extract(
+    element_at(filter(P.identifier, x -> x.system = 'http://openelis-global.org/facility_id'), 1).assigner.reference,
+    'Organization/(.*)',
+    1
+  ) AS facility_org_id
 FROM Patient AS P LATERAL VIEW OUTER explode(name) AS PN
   LATERAL VIEW OUTER explode(PN.given) AS PNG
   LATERAL VIEW OUTER explode(P.address) AS PA
